2fcd50d9beb16771b9a13b6274a00cd5fab303d0
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2021-08-13 12:50:44 -0400

    found average imdb rating

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function getHighestMetascore(movies) {
   //loop
   for(let i = 0; i < movies.length; i++){
     if(currentMovie < movies[i].metascore){
-    currentMovie = movies[i].metascore; //redeclared largest
+    currentMovie = movies[i].metascore; //redeclared variable
     }
   }
   return Number(currentMovie);
@@ -81,14 +81,16 @@ function getHighestMetascore(movies) {
  */
 function getAverageIMDBRating(movies) {
   if(movies.length === 0){
-    return 0;
+    return 0; // if movies array is empty return 0
   }
 
-  let avg = 0;
-  for(let i = 0; i < movies.length; i++){
-    avg = movies[i].imdbRating / movies.length
-    return Number(avg.toFixed(2))
+  let avg = 0; //default value for average
+  for(let i = 0; i < movies.length; i++){ //loop
+    movie = movies[i];
+    avg += movie.imdbRating/movies.length; // how to calculate avg
+    
   }
+  return avg //number being returned
 }
 
 /**
